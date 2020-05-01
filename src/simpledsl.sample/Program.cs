1ad6ca3e72dd9c75fdb7373a5cd33841c8d862c6
R. G. Esteves <rgesteves@gmail.com>
2020-05-01 02:37:17 -0700

    Initial import

---
+++ src/simpledsl.sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using simpledsl;
+
+// This is a sample of a very basic internal DSL
+
+namespace simpledsl.sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node n = new Node();
+            Console.WriteLine($"Checking that the new child has {n.ChildCount} children!");
+        }
+    }
+}
---
+++ src/simpledsl/Node.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace simpledsl
+{
+    public class Node
+    {
+        public int ChildCount { get {return 0;}}
+    }
+}
